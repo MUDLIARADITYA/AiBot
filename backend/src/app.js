27a179b1af7677e3fa0512e48f02cd851a114863
Aditya <mudliaraditya@gmail.com>
2025-12-07 21:48:53 +0530

    some code added

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,14 @@
 const express =require("express")
+const path = require('path')
 
 const app = express();
+app.use(express.static(path.join(__dirname,'../public')));
 
 app.get("/",(req,res)=>{
     res.send("hello world")
 })
 
+app.get("name",(req,res)=>{
+    res.sendFile(this.path.join(__dirname, '../public/index.html'))
+})
 module.exports = app;
